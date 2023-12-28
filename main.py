ae652c35db2c475e810e460adb96837b67cef5d5
binayrijal <binayrijal22@gmail.com>
2023-12-28 21:05:28 +0545

    all is done

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from flask import  Flask,render_template,request
 import pandas as pd
 import numpy as np
-
+import pickle
 
 app=Flask(__name__)
 car=pd.read_csv("processed_data.csv")
+model=pickle.load(open("LinearRegressionmodel.csv",'rb'))
 
 @app.route('/')
 def home():
@@ -15,16 +16,16 @@ def home():
     return render_template('index.html',company=company,car_models=car_models,year=year,fuel_type=fuel_type)
 
 
-@app.route('/predict')
+@app.route('/predict', methods=['POST'])
 def predict():
     company=request.form.get('company')
     car_models=request.form.get('car_models')
     year = int(request.form.get('year'))
     fuel_type = request.form.get('fuel_type')
-    kms_driven=int(request.form.get('kms_driven'))
-    print(company,car_models,year,fuel_type,kms_driven)
+    kms_driven = int(request.form.get('kms_driven'))
+    prediction = model.predict(pd.DataFrame([[car_models,company,year,kms_driven,fuel_type]],columns=['name','company','year','kms_driven','fuel_type']))
 
-    return ""
+    return str(np.round(prediction[0],2))
 
 
 
